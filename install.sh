7b7009febba7fb81c6b0bcf05e6878fc3478a28a
Brendon Le Comte <brendonlc@graphcore.ai>
2020-05-13 17:14:05 +0100

    [INSTALL] tidy up symbolic link locations for ubuntu

--- install.sh
+++ install.sh
@@ -3,17 +3,18 @@
 ln -sf $PWD/bashrc ~/.bashrc
 ln -sf $PWD/bash_profile ~/.bash_profile
 ln -sf $PWD/vimrc ~/.vimrc
-ln -sf $PWD/config ~/.config
 
 if [ "$(uname)" == "Darwin" ]; then
   ln -sf $PWD/tmux.conf.osx ~/.tmux.conf
   brew install tmux
   brew install python3-pip
   brew install fish
+  ln -sf $PWD/config ~/.config
 else
   deb_packages="tmux python3-pip fish"
   sudo apt -y install ${deb_packages}
-  ln -sf $PWD/tmux.conf.linux ~/.tmux.conf
+  ln -sf $PWD/tmux.conf ~/.tmux.conf
+  ln -sf $PWD/config/fish/config.fish ~/.config/fish/config.fish
 fi
 
 pip3 install powerline-status
