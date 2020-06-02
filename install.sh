a999205c27a815fb64eaa78b71dda3cf5e59844c
Brendon Le Comte <brendonlc@graphcore.ai>
2020-06-02 09:11:43 +0100

    [INSTALL] install clangd, tidy and comment install script

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-
+# Install required tools
+# OS dependant config folder links
 if [ "$(uname)" == "Darwin" ]; then
   ln -sf $PWD/tmux.conf.osx ~/.tmux.conf
   brew install tmux
@@ -8,9 +9,10 @@ if [ "$(uname)" == "Darwin" ]; then
   brew install fish
   brew install neovim
   brew install node
+  brew install llvm
   ln -sf $PWD/config ~/.config
 else
-  deb_packages="tmux python3-pip fish neovim nodejs npm"
+  deb_packages="tmux python3-pip fish neovim nodejs npm clangd-9"
   sudo apt -y install ${deb_packages}
   ln -sf $PWD/tmux.conf ~/.tmux.conf
   ln -sf $PWD/config/fish ~/.config/fish
@@ -18,10 +20,14 @@ else
   ln -sf $PWD/config/vim ~/.config/vim
   ln -sf $PWD/config/nvim ~/.config/nvim
 fi
+
+# Link key files and folders
 ln -sf $PWD/profile ~/.profile  
 ln -sf $PWD/vimrc ~/.vimrc
 ln -sf $PWD/tools ~/.tools
 
+# Install the powerline display bar
 pip3 install powerline-status
 
+# Install Kitty. GPU powered terminal 
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
