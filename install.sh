c79de93f437fc220381a45405b587086b7f07ac1
Brendon Le Comte <brendon.lecomte@gmail.com>
2021-03-19 10:34:04 +1300

    [INSTALL] use script path not PWD for linking

--- install.sh
+++ install.sh
@@ -5,28 +5,27 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 # Install required tools
 # OS dependant config folder links
 if [ "$(uname)" == "Darwin" ]; then
-  ln -sf $PWD/tmux.conf.osx ~/.tmux.conf
   brew install tmux
   brew install python3-pip
   brew install fish
   brew install neovim
   brew install node
   brew install llvm
-  ln -sf $PWD/config ~/.config
+  ln -sf $SCRIPTPATH/config ~/.config
 else
   deb_packages="tmux python3-pip fish neovim nodejs npm clangd-9 powerline-fonts"
   sudo apt -y install ${deb_packages}
-  ln -sf $PWD/tmux.conf ~/.tmux.conf
-  ln -sf $PWD/config/fish ~/.config/fish
-  ln -sf $PWD/config/kitty ~/.config/kitty
-  ln -sf $PWD/config/vim ~/.config/vim
-  ln -sf $PWD/config/nvim ~/.config/nvim
+  ln -sf $SCRIPTPATH/config/fish ~/.config/fish
+  ln -sf $SCRIPTPATH/config/kitty ~/.config/kitty
+  ln -sf $SCRIPTPATH/config/vim ~/.config/vim
+  ln -sf $SCRIPTPATH/config/nvim ~/.config/nvim
 fi
 
 # Link key files and folders
-ln -sf $PWD/profile ~/.profile
-ln -sf $PWD/vimrc ~/.vimrc
-ln -sf $PWD/tools ~/.tools
+ln -sf $SCRIPTPATH/profile ~/.profile
+ln -sf $SCRIPTPATH/vimrc ~/.vimrc
+ln -sf $SCRIPTPATH/tools ~/.tools
+ln -sf $SCRIPTPATH/tmux.conf ~/.tmux.conf
 
 # Install the powerline display bar
 pip3 install powerline-status
